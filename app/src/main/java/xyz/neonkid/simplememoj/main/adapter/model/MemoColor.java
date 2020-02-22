3843e0f7f77ccd530dc8019f848c131e0e893a8d
NEONKID <clax1412@outlook.com>
2020-02-22 14:19:44 +0900

    Define Memo Color Model...

---
+++ app/src/main/java/xyz/neonkid/simplememoj/main/adapter/model/MemoColor.java
@@ -0,0 +1,17 @@
+package xyz.neonkid.simplememoj.main.adapter.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by Neon K.I.D on 2/20/20
+ *
+ * Blog : https://blog.neonkid.xyz
+ * Github : https://github.com/NEONKID
+ */
+public class MemoColor implements Serializable {
+    public String color;
+
+    public MemoColor(String color) {
+        this.color = color;
+    }
+}
