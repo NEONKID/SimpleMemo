85c5f5aa76203dccb9cfbf5b32540f6cf333520e
NEONKID <clax1412@outlook.com>
2020-03-23 17:54:16 +0900

    Add Error Handler..

--- app/src/main/java/xyz/neonkid/simplememoj/SimpleMemoApplication.java
+++ app/src/main/java/xyz/neonkid/simplememoj/SimpleMemoApplication.java
@@ -4,12 +4,15 @@ import android.app.Application;
 
 import io.realm.Realm;
 import xyz.neonkid.simplememoj.main.util.RealmHelper;
+import xyz.neonkid.simplememoj.main.util.AppExceptionHandler;
 
 public class SimpleMemoApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         Realm.init(this);
+
+        setCrashHandler();
     }
 
     @Override
@@ -19,5 +22,19 @@ public class SimpleMemoApplication extends Application {
         Realm realm = RealmHelper.getInstance().getRealmInstance();
         if (!realm.isClosed())
             realm.close();
+    }
+
+    private void setCrashHandler() {
+        Thread.UncaughtExceptionHandler defaultExceptionHandler
+                = Thread.getDefaultUncaughtExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler((t, e) -> {});
+
+        Thread.UncaughtExceptionHandler fabricExceptionHandler
+                = Thread.getDefaultUncaughtExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler(new AppExceptionHandler(
+                this,
+                defaultExceptionHandler,
+                fabricExceptionHandler
+        ));
     }
 }
